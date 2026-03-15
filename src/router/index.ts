65c9807302a1847b5d7927491bc9cb579a577ca6
timothy okooboh <okoobohtimothy@gmail.com>
2026-03-15 06:12:34 +0100

    feat: setup routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      name: 'HomePage',
+      path: '/',
+      component: () => import('@/views/HomePage.vue'),
+    },
+  ],
 })
 
 export default router

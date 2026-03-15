a5a829f878037e39aa5258c802d5edf7fecf456a
timothy okooboh <okoobohtimothy@gmail.com>
2026-03-15 06:13:32 +0100

    feat: create the entry component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+
+import 'vue-sonner/style.css'
+import { Toaster } from '@/components/ui/sonner'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="max-w-400 mx-auto">
+    <TheHeader />
+    <RouterView />
+    <Toaster rich-colors />
+  </div>
 </template>
-
-<style scoped></style>

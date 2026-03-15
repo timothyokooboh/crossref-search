bfb6836e1749a806ec435464463e8f151d8a453f
timothy okooboh <okoobohtimothy@gmail.com>
2026-03-15 06:33:47 +0100

    types: set the response type for the API request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
-import type { SearchParams } from '@/types'
+import type { ApiResponse, SearchParams } from '@/types'
 import { http } from './http'
 
-export const searchWorks = async (params: SearchParams, signal: AbortSignal) => {
+export const searchWorks = async (
+  params: SearchParams,
+  signal: AbortSignal,
+): Promise<null | ApiResponse> => {
   if (!params['query.bibliographic']) return null
   return http.get('/works', { params, signal })
 }

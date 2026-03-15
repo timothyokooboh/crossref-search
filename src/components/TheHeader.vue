dfdd8deab1fd24ee2d67faa34d223f4f72e89d53
timothy okooboh <okoobohtimothy@gmail.com>
2026-03-15 06:08:41 +0100

    feat: create the header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="flex items-center h-16 px-3 sm:px-5 bg-white shadow">
+    <img src="/crossref-logo.png" alt="" class="object-contain" />
+
+    <div class="flex flex-1 justify-center">
+      <SearchInput class="flex-1" />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import SearchInput from './SearchInput.vue'
+</script>

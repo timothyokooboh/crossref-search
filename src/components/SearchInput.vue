458d06490bc86c5cb230d89d20fd26ebd4753b8a
timothy okooboh <okoobohtimothy@gmail.com>
2026-03-15 06:10:10 +0100

    feat: create search input component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex relative max-w-50 sm:max-w-75 md:max-w-125">
+    <SearchIcon class="size-4 absolute top-2.5 left-2 text-[#94A3B8]" />
+    <Input
+      aria-label="Search title, author, DOIs, ORCIDs"
+      placeholder="Search title, author, DOIs, ORCIDs..."
+      class="flex-1 pl-9 text-sm border-0 bg-[#F1F5F9] text-[#6B7280]"
+      v-model="searchQuery"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { useSearchStore } from '@/store/useSearchStore'
+import { SearchIcon } from 'lucide-vue-next'
+import { storeToRefs } from 'pinia'
+
+const { searchQuery } = storeToRefs(useSearchStore())
+</script>

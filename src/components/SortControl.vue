7afbdca79d7c71fb9b8fb021ab33bcdf381af09f
timothy okooboh <okoobohtimothy@gmail.com>
2026-03-15 06:10:34 +0100

    feat: create components for sorting

---
+++ src/components/SortControl.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center gap-2">
+    <span class="text-sm text-[#64748B]">Sorted by:</span>
+
+    <Tabs :default-value="sortBy">
+      <TabsList>
+        <TabsTrigger value="relevance" @click="sortBy = 'relevance'"> Relevance </TabsTrigger>
+        <TabsTrigger value="published" @click="sortBy = 'published'"> Year </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { useSearchStore } from '@/store/useSearchStore'
+import { storeToRefs } from 'pinia'
+
+const { sortBy } = storeToRefs(useSearchStore())
+</script>
